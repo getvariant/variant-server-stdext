d3537e3130ad7b3f84afcda07e6674528cebec23
iurisman <igor.urisman@gmail.com>
2017-07-06 15:16:30 -0700

    Move out of the SERVER project.

---
+++ server-extensions-demo/src/main/java/com/variant/server/ext/demo/ChromeTargetingHook.java
@@ -0,0 +1,50 @@
+package com.variant.server.ext.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.typesafe.config.Config;
+import com.variant.core.UserHook;
+import com.variant.core.schema.Hook;
+import com.variant.core.schema.Test.Experience;
+import com.variant.server.api.Session;
+import com.variant.server.api.hook.PostResultFactory;
+import com.variant.server.api.hook.TestTargetingLifecycleEvent;
+import com.variant.server.api.hook.TestTargetingLifecycleEventPostResult;
+
+
+/**
+ * User hook to target traffic from chrome browsers to control.
+ * Users with Chrome browsers will participate in the experiment, but always routed to control.
+ */
+public class ChromeTargetingHook implements UserHook<TestTargetingLifecycleEvent> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ChromeTargetingHook.class);
+	
+	@Override
+	public void init(Config config, Hook hook) throws Exception {
+		// No configuration.
+	}
+
+	@Override
+	public Class<TestTargetingLifecycleEvent> getLifecycleEventClass() {
+		return TestTargetingLifecycleEvent.class;
+	}
+
+	@Override
+	public UserHook.PostResult post(TestTargetingLifecycleEvent event) throws Exception {
+
+		Session ssn = event.getStateRequest().getSession();
+		LOG.debug(ssn.getAttribute("user-agent"));
+		if (ssn.getAttribute("user-agent").matches(".*Chrome.*")) {
+			Experience exp = event.getTest().getControlExperience();
+			LOG.debug("Targeted Chrome session [" + ssn.getId() + "] to experience [" + exp.getName() + "] in test [" + event.getTest().getName() + "]");
+			TestTargetingLifecycleEventPostResult result = PostResultFactory.mkPostResult(event);
+			result.setTargetedExperience(exp);
+			return result;
+		}
+		// Other qual hooks may still disqualify.
+		else return null;
+	}
+
+}
---
+++ server-extensions-demo/src/main/java/com/variant/server/ext/demo/FirefoxDisqualHook.java
@@ -0,0 +1,47 @@
+package com.variant.server.ext.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.typesafe.config.Config;
+import com.variant.core.UserHook;
+import com.variant.core.schema.Hook;
+import com.variant.server.api.Session;
+import com.variant.server.api.hook.PostResultFactory;
+import com.variant.server.api.hook.TestQualificationLifecycleEvent;
+import com.variant.server.api.hook.TestQualificationLifecycleEventPostResult;
+
+/**
+ * User hook to disqualify traffic from Firefox browsers.
+ * Users with Firefox browsers will not participate in the experiment.
+ */
+public class FirefoxDisqualHook implements UserHook<TestQualificationLifecycleEvent> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(FirefoxDisqualHook.class);
+	
+	@Override
+	public void init(Config config, Hook hook) throws Exception {
+		// No configuration.
+	}
+
+	@Override
+	public Class<TestQualificationLifecycleEvent> getLifecycleEventClass() {
+		return TestQualificationLifecycleEvent.class;
+	}
+
+	@Override
+	public UserHook.PostResult post(TestQualificationLifecycleEvent event) throws Exception {
+
+		Session ssn = event.getStateRequest().getSession();
+		LOG.debug(ssn.getAttribute("user-agent"));
+		if (ssn.getAttribute("user-agent").matches(".*Firefox.*")) {
+			LOG.debug("Disqualified Safari session [" + ssn.getId() + "]");
+			TestQualificationLifecycleEventPostResult result = PostResultFactory.mkPostResult(event);
+			result.setQualified(false);
+			return result;
+		}
+		// Other qual hooks may still disqualify.
+		else return null;
+	}
+
+}
