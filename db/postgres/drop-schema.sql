270f7a2658619f895753c706e4e54191843125e1
Igor <>
2019-05-13 21:28:36 -0700

    Remove sequences.

--- db/postgres/drop-schema.sql
+++ db/postgres/drop-schema.sql
@@ -1,11 +1,7 @@
 DROP VIEW events_v;
 
 DROP TABLE events CASCADE;
-
-DROP SEQUENCE events_id_seq;
  
 DROP TABLE event_experiences CASCADE;
 
 DROP TABLE event_attributes;
-
-DROP SEQUENCE event_experiences_id_seq;
