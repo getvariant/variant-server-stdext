50572c59eddee7ae6fdb8c2113b3becaa8fb0c9b
Igor <>
2019-07-01 11:19:52 -0700

    Fix title.

--- bin/javadoc.sh
+++ bin/javadoc.sh
@@ -14,7 +14,7 @@ rm -rf ${javadoc_dir}/*
 javadoc -d ${javadoc_dir}  --allow-script-in-comments \
    -sourcepath ${root_dir}/src/main/java \
    -windowtitle "Variant ${version}" \
-   -doctitle "Variant Experiment Server Standard Extensions" \
+   -doctitle "Variant Application Iteration Server Standard Extension Library" \
    -header "<a onclick=\"window.top.location.href='http://getvariant.com';\" href=\"#\"> <img style=\"margin-bottom:5px;\" src=\"http://getvariant.com/wp-content/uploads/2016/05/VariantLogoSmall.png\"/> \</a>" \
    -bottom "Release $version. Updated $(date +"%d %b %Y").<br/> Copyright &copy; 2019 <a onclick=\"window.top.location.href='http://getvariant.com';\" href=\"#\">Variant Inc.</a>" \
    -subpackages \
