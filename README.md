6560f6a47d6d013c664500710add1a8166cbba91
Igor Urisman <igor@Igors-MacBook-Pro.local>
2023-08-17 15:28:41 -0700

    WIP

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# Variant АIM Server Standard Extension Library
-### Release 0.10.0
-#### Requires: Java 8 or later
-
-[__Documentation__](https://www.getvariant.com/resources/docs/0-10/application-iteration-server/reference/#section-4) | [__Javadoc__](https://getvariant.github.io/variant-extapi-standard/)
+# Variant CVM Server SPI Standard Library
+### Release 1.0.0
--- bin/release.sh
+++ bin/release.sh
@@ -13,9 +13,9 @@ mvn clean package
 
 # Un-package the JAR and add the /db directory to it. /db directory contains DB related SQL and shell
 # scripts that are used by tests.
-jar -uf target/variant-extapi-standard-${version}.jar $(find db)
+jar -uf target/variant-spi-stdlib-${version}.jar $(find db)
 
 # Copy the new build into the server's distribution directory. Assuming the location of the variant
 # local repo.
-cp target/variant-extapi-standard-${version}.jar ../variant/SERVER/src/universal/ext
-cp target/variant-extapi-standard-${version}.jar ../variant/SERVER/lib
+cp target/variant-spi-stdlib-${version}.jar ../variant/SERVER/src/universal/ext
+cp target/variant-spi-stdlib-${version}.jar ../variant/SERVER/lib
